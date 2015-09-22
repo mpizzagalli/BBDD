ea857480dec221102b727bb82d5f2b580ad05b99
gastonrocca <gastonrocca@gmail.com>
2015-09-22 00:35:36 -0300

    Modificacion Consulta 2

--- bd/Consultas/DetalleDeAccidentesPorModalidad.sql
+++ bd/Consultas/DetalleDeAccidentesPorModalidad.sql
@@ -1,11 +1,20 @@
+USE `tp1`;
+DROP procedure IF EXISTS `DetalleDeAccidentesPorModalidad`;
+
+DELIMITER $$
+USE `tp1`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DetalleDeAccidentesPorModalidad`(modalidad varchar(200))
 BEGIN
 
-select Licencia.NroLicencia, count(idSiniestro)
+select Licencia.NroLicencia, count(Siniestro.idSiniestro)
 from TipoColision
 inner join Siniestro on Siniestro.idTipoColision = TipoColision.idTipoColision 
 inner join siniestrovehiculoconductor SVC on SVC.idSiniestro = Siniestro.idSiniestro 
-inner join Conductor on SVN.DNI = Conductor.DNI
-inner join Licencia on Conductor.DNI = Licencia.DNI
-where tipocolision.Descripcion = modalidad;
-END
+inner join Conductor on  Conductor.DNI = SVC.DNI 
+inner join Licencia on Conductor.nroLicencia = Licencia.nroLicencia
+where TipoColision.Descipcion = modalidad
+group by Licencia.NroLicencia;
+
+END$$
+
+DELIMITER ;
