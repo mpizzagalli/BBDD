576e7fe010611235f6b85edbe414114b962adc2e
gitnac <n4critico@hotmail.com>
2015-09-22 21:02:13 -0300

    agrego un par de dumps basicos

---
+++ DATA_DUMPS/dump._otros.sql
@@ -0,0 +1,76 @@
+INSERT INTO `tipolicencia` VALUES (1,"B1", "Comun");
+INSERT INTO `tipolicencia` VALUES (2,"B2", "Professional");
+INSERT INTO `tipolicencia` VALUES (3,"C1", "Motos");
+INSERT INTO `tipolicencia` VALUES (4,"H1","Transporte");
+INSERT INTO `tipolicencia` VALUES (5,"H2","Transporte con acoplado");
+
+INSERT INTO `tipoinforme` VALUES (1,"Informe fiscal.");
+INSERT INTO `tipoinforme` VALUES (2,"Informe de daños.");
+INSERT INTO `tipoinforme` VALUES (3,"Registro de licencias");
+INSERT INTO `tipoinforme` VALUES (4,"Informe balistico");
+INSERT INTO `tipoinforme` VALUES (5,"Informe de alcholemia y farmacologico");
+
+INSERT INTO `tipovehiculo` VALUES (1,"AUTO");
+INSERT INTO `tipovehiculo` VALUES (2,"MOTO");
+INSERT INTO `tipovehiculo` VALUES (3,"CAMION");
+INSERT INTO `tipovehiculo` VALUES (4,"TRACTOR");
+INSERT INTO `tipovehiculo` VALUES (5,"BICICLETA");
+INSERT INTO `tipovehiculo` VALUES (6,"COLECTIVO");
+INSERT INTO `tipovehiculo` VALUES (7,"4X4");
+INSERT INTO `tipovehiculo` VALUES (8,"MICRO");
+INSERT INTO `tipovehiculo` VALUES (9,"COMBI");
+
+INSERT INTO `tipocobertura` VALUES (1,"Basica");
+INSERT INTO `tipocobertura` VALUES (2,"Contra terceros");
+INSERT INTO `tipocobertura` VALUES (3,"Completa");
+INSERT INTO `tipocobertura` VALUES (4,"Premium");
+INSERT INTO `tipocobertura` VALUES (5,"Ninguna");
+
+
+INSERT INTO `tipoinfraccion` VALUES (1,"Exceso de velocidad");
+INSERT INTO `tipoinfraccion` VALUES (2,"Contra-mano");
+INSERT INTO `tipoinfraccion` VALUES (3,"Senda indebida");
+INSERT INTO `tipoinfraccion` VALUES (4,"Mal uso de banquina");
+
+
+
+INSERT INTO `tipocolision` VALUES (1,"Choque de frente");
+INSERT INTO `tipocolision` VALUES (2,"Choque multiple/cadena");
+INSERT INTO `tipocolision` VALUES (3,"Golpe lateral");
+INSERT INTO `tipocolision` VALUES (4,"Choque a peaton");
+INSERT INTO `tipocolision` VALUES (5,"Choque contra bicicleta");
+INSERT INTO `tipocolision` VALUES (6,"Choque contra guardrail");
+
+
+
+INSERT INTO `tipoaccidente` VALUES (1,"Leve");
+INSERT INTO `tipoaccidente` VALUES (2,"Grave");
+INSERT INTO `tipoaccidente` VALUES (3,"Fatal");
+
+
+
+INSERT INTO `estadoiluminacion` VALUES (1,"Excelente");
+INSERT INTO `estadoiluminacion` VALUES (2,"Bueno");
+INSERT INTO `estadoiluminacion` VALUES (3,"Regular");
+INSERT INTO `estadoiluminacion` VALUES (4,"Pobre");
+INSERT INTO `estadoiluminacion` VALUES (5,"Trampa mortal");
+
+INSERT INTO `tipoestadovia` VALUES (1,"Excelente");
+INSERT INTO `tipoestadovia` VALUES (2,"Bueno");
+INSERT INTO `tipoestadovia` VALUES (3,"Regular");
+INSERT INTO `tipoestadovia` VALUES (4,"Pobre");
+INSERT INTO `tipoestadovia` VALUES (5,"Trampa mortal");
+
+
+
+INSERT INTO `tipocondicionclimatica` VALUES (1,"Soleado");
+INSERT INTO `tipocondicionclimatica` VALUES (2,"Nublado");
+INSERT INTO `tipocondicionclimatica` VALUES (3,"Lluvioso");
+INSERT INTO `tipocondicionclimatica` VALUES (4,"Con niebla");
+INSERT INTO `tipocondicionclimatica` VALUES (5,"Tormenta");
+INSERT INTO `tipocondicionclimatica` VALUES (6,"Tormenta de nieve");
+
+
+INSERT INTO `tipopavimento` VALUES (1,"Asfalto");
+INSERT INTO `tipopavimento` VALUES (2,"Tierra");
+INSERT INTO `tipopavimento` VALUES (3,"Empedrado");
