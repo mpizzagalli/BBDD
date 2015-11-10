ecbbc4c8085f0d23fadea3864951da5d8eb2efca
Matias Pizzagalli <matipizza@gmail.com>
2015-11-10 19:49:15 -0300

    mediciones del sharding.

--- tp2/insert_data.js
+++ tp2/insert_data.js
@@ -17,4 +17,31 @@ var insertData = function(dbName, colName, num) {
   }
 
   print(col.count());
+  print(col.getShardDistribution());
+  print("#########################################################################");
+}
+
+var insertDataTotal = function(dbName, colName, step, total) {
+
+  var col = db.getSiblingDB(dbName).getCollection(colName);
+
+  while(col.count() < total){
+    for (i = 0; i < step; i++) {
+      x = Math.floor(Math.random() * 1000000);
+      doc = {
+        id: i,
+        nombre: 'Martin Juarez',
+        password: 'asdasd' ,
+        codigo_postal: x,
+        genero: 'masculino',
+        edad: 29,
+        fecha_creacion: '30/02/2015'
+      }
+      col.insert(doc);
+    }
+
+    print(col.count());
+    print(col.getShardDistribution());
+    print("#########################################################################");
+  }
 }
